3c6c87e44b0e5a61d0287770692f88a9db797f0d
Netto Querois <24768847+nettoquerois@users.noreply.github.com>
2021-11-30 05:33:07 +0000

    completo!!!!!!

--- js/main.js
+++ js/main.js
@@ -1,12 +1,31 @@
-(function ($) {
-    'use strict';
+$(document).ready(function () {
+  $('.slider').slick({
+    lazyLoad: 'ondemand',
+    arrows: true,
+    dots: true,
+    infinite: true,
+    swipeToSlide: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+      responsive: [
+    {
+      breakpoint: 576,
+      settings: {
+        arrows: false,
+        dots: false,
+        slidesToShow: 1,
+        slidesToScroll: 1
+      }
+    }
+  ]
+  });
 
-$(window).load(function() {
+  const _yearElementId = $('#current-year');
+  const _fullYear = new Date();
+  _yearElementId.text(_fullYear.getFullYear());
 });
-
-
-})(jQuery);
-
 
 
 
